235e02ff2c2fed621b3d373a6a47c83fbbdeb8dd
Brandon Duong <brduong@cisco.com>
2026-07-15 13:48:51 -0700

    update gitignore and have basic mcp server

--- py/1Hello/server.py
+++ py/1Hello/server.py
@@ -1,3 +1,17 @@
 from mcp.server.fastmcp import FastMCP
+from quotes import get_sad_quote, get_happy_quote
 
 mcp = FastMCP("AXBV-quotes-server")
+
+@mcp.tool(
+    name="Get-AXBV-happy-quote"
+)
+def get_axbv_happy_quote() -> str:
+    return get_happy_quote()
+
+@mcp.tool(name="Get-AXBV-sad-quote")
+def get_axbv_sad_quote() -> str:
+    return get_sad_quote()
+
+if __name__ == "__main__":
+    mcp.run()
