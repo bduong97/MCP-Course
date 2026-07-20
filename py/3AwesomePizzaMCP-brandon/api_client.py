96aebc7277141621602874402d12ec66629b1aba
Brandon Duong <brduong@cisco.com>
2026-07-20 13:42:18 -0700

    implement a couple other tools

--- py/3AwesomePizzaMCP-brandon/api_client.py
+++ py/3AwesomePizzaMCP-brandon/api_client.py
@@ -1,5 +1,5 @@
 import requests
-
+from typing import TypedDict, List
 BASE_URL = "http://localhost:3000/api"
 
 
@@ -10,8 +10,8 @@ def get_daily_menu():
             headers={"Accept": "application/json", "Content-Type": "application/json"},
         )
         resp.raise_for_status()
-        data = resp.json()
-        return data
+        menu = resp.json()['data']
+        return menu
     except requests.exceptions.RequestException as e:
         return {"success": False, "message": str(e), "data": []}
     except Exception as error:
@@ -21,3 +21,49 @@ def get_daily_menu():
             "data": [],
         }
 
+class OrderItem (TypedDict):
+    '''
+    Type definition for order item
+    '''
+    name: str
+    quantity: int
+
+class OrderDetails(TypedDict):
+    sender: str
+    contents: List[OrderItem]
+
+def make_order(order_details: OrderDetails) -> dict:
+    try:
+        response = requests.post(
+            f"{BASE_URL}/orders",
+            headers={"Accept": "application/json", "Content-Type": "application/json"},
+            json=order_details
+        )
+        response.raise_for_status()
+        response_json = response.json()
+        return {
+            'order_id': response_json['data']['id']
+        }
+    except Exception as error:
+        return {
+            'success': False,
+            'message': f'Unknown error occured: {str(error)}',
+            'order_id': ''
+        }
+
+def check_order_status(order_id: str) -> str:
+    try:
+        response = requests.get(
+            f"{BASE_URL}/orders/{order_id}",
+            headers={"Accept": "application/json", "Content-Type": "application/json"}
+        )
+        response.raise_for_status()
+        response_json = response.json()
+        if response_json.get('success'):
+            return response_json['data']['status']
+        else:
+            return response_json.get('message', 'Unknown error')
+    except requests.exceptions.RequestException as error:
+        return str(error)
+    except Exception as error:
+        return f"Unknown error occured: {str(error)}"
--- py/3AwesomePizzaMCP-brandon/server.py
+++ py/3AwesomePizzaMCP-brandon/server.py
@@ -1,8 +1,45 @@
-from mcp.server.fastmcp import FastMCP
+import base64
 
+import requests
+from mcp.server.fastmcp import FastMCP
+from typing import List
+from mcp.types import CallToolResult, ImageContent, TextContent
+from api_client import OrderItem
 mcp = FastMCP("Awesome pizza MCP")
 
 
-@mcp.tool(name="Test tool", description="Test tool description")
+@mcp.tool(name="test_tool", description="Test tool description")
 def say_hello() -> str:
     return "Hello from MCP!"
+
+
+@mcp.tool(name="get_pizza_menu_with_images", description="Get the daily pizza menu")
+def get_pizza_menu_with_images() -> CallToolResult:
+    from api_client import get_daily_menu
+
+    menu = get_daily_menu()
+    content = []
+    for item in menu:
+        content.append(TextContent(type="text", text=item["name"]))
+        content.append(TextContent(type="text", text=item["description"]))
+        image_url = f"http://localhost:3000/{item['imageUrl']}"
+        image_response = requests.get(image_url)
+
+        img_data = base64.b64encode(image_response.content).decode("utf-8")
+        content.append(ImageContent(type="image", data=img_data, mimeType="image/png"))
+    return CallToolResult(content=content)
+
+@mcp.tool(name="make_order", description="Make an order at awesome pizza; Returns an order ID")
+def make_order(items: List[OrderItem]) -> str:
+    from api_client import make_order
+    order_response = make_order({
+        "sender": "MCP user",
+        "contents": items
+    })
+    return  order_response['order_id']
+
+@mcp.tool(name="check_order_status", description="Check the status of an order by its id")
+def check_order_status(order_id: str) -> str:
+    from api_client import check_order_status
+    order_status = check_order_status(order_id)
+    return order_status
